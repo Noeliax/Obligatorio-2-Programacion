2ccd0343893540f984daf48a88656bd9d8834140
Camilo Ramírez <camiloram18042003@gmail.com>
2025-06-13 20:37:43 -0300

    Refactor form elements and enhance system functionality for managing carreras, patrocinadores, and corredores

--- js/clases.js
+++ js/clases.js
@@ -1,17 +1,83 @@
 // Camilo Ramírez (363781) Noelia López (358582)
-
 class Sistema {
+  constructor () {
+    this.carreras = [];
+    this.corredores = [];
+    this.patrocinadores = [];
+    this.inscripciones = [];
+  }
+
+  agregarCarrera(carrera) {
+    let esValida = true;
+    for (let car of this.carreras) {
+      if (car.nombre === carrera.nombre) {
+        esValida = false;
+      }
+    }
+    if (esValida) {
+      this.carreras.push(carrera)
+    }
+  }
+
+  agregarPatrocinador(patrocinador) {
+    let index = -1;
+    for (let pat of this.patrocinadores) {
+      if (pat.nombre === patrocinador.nombre) {
+        index = this.patrocinadores.indexOf(pat);
+      }
+    }
+    if (index === -1) {
+      this.patrocinadores.push(patrocinador);
+    } else {
+      this.patrocinadores[index].rubro = patrocinador.rubro;
+      this.patrocinadores[index].carreras = patrocinador.carreras;
+    }
+  }
+
+  agregarCorredor(corredor) {
+    let esValido = true;
+    for (let cor of this.corredores) {
+      if (cor.cedula === corredor.cedula) {
+        esValido = false;
+      }
+    }
+    if (esValido) {
+      this.corredores.push(corredor);
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
+
 class Carrera {
-
+  constructor (nombre, departamento, fecha, cupo) {
+    this.nombre = nombre;
+    this.departamento = departamento;
+    this.fecha = fecha;
+    this.cupo = cupo;
+  }
 }
-class Corredor {
 
+class Patrocinador {
+  constructor (nombre, rubro, carreras) {
+    this.nombre = nombre;
+    this.rubro = rubro;
+    this.carreras = carreras;
+  }
+}
+
+class Corredor {
+  constructor (nombre, edad, cedula, fechaFichaMedica, tipoDeCorredor) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.cedula = cedula;
+    this.fechaFichaMedica = fechaFichaMedica;
+    this.tipoDeCorredor = tipoDeCorredor;
+  }
 }
 class Inscripcion {
 
 }
-class Patrocinador {
 
-}
--- js/funciones.js
+++ js/funciones.js
@@ -1,13 +1,14 @@
 // Camilo Ramírez (363781) Noelia López (358582)
 window.addEventListener("load",config);
-
-var seccionActual = "datos_seccion";
+let sistema = new Sistema();
+let seccionActual = "datos_seccion";
 
 function config(){
-  // eventos
   document.getElementById("boton_datos").addEventListener("click", cambiarSeccionDatos);
   document.getElementById("boton_estadisticas").addEventListener("click", cambiarSeccionEstadisticas);
-
+  document.getElementById("boton_carrera").addEventListener("click", agregarCarrera);
+  document.getElementById("boton_patrocinador").addEventListener("click", agregarPatrocinador);
+  document.getElementById("boton_corredor").addEventListener("click", agregarCorredor);
 }
 
 // Darle una vuelta
@@ -30,3 +31,67 @@ function cambiarSeccionEstadisticas() {
     seccionActual = "estadisticas_seccion";
   }
 }
+
+function agregarElementoCarrera(nombre) {
+  let selectPat = document.getElementById("carreras_patrocinador");
+  let selectIns = document.getElementById("carrera_inscripcion");
+  let opcion = document.createElement("option");
+  opcion.value = nombre;
+  opcion.textContent = nombre;
+
+  selectPat.appendChild(opcion);
+  selectIns.appendChild(opcion.cloneNode(true));
+}
+
+function agregarCarrera() {
+  let nombre = document.getElementById("nombre_carrera").value;
+  let departamento = document.getElementById("departamento_carrera").value;
+  let fecha = document.getElementById("fecha_carrera").value;
+  let cupo = document.getElementById("cupo").value;
+
+  let carrera = new Carrera(nombre, departamento, fecha, cupo);
+  sistema.agregarCarrera(carrera)
+  agregarElementoCarrera(nombre)
+}
+
+function agregarPatrocinador() {
+  let nombre = document.getElementById("nombre_patrocinador").value;
+  let rubro = document.getElementById("rubro").value;
+  let carreraElementos = document.getElementById("carreras_patrocinador").selectedOptions;
+  let carreras = [];
+  for (let elemento of carreraElementos) {
+    carreras.push(elemento.value);
+  }
+  let patrocinador = new Patrocinador(nombre, rubro, carreras);
+  sistema.agregarPatrocinador(patrocinador);
+}
+
+function agregarElementoCorredor(nombre, cedula) {
+  let selectCorredor = document.getElementById("corredor_inscripcion");
+  let opcion = document.createElement("option");
+  opcion.value = cedula;
+  opcion.textContent = nombre;
+
+  selectCorredor.appendChild(opcion);
+}
+
+function agregarCorredor() {
+  let nombre = document.getElementById("nombre_corredor").value;
+  let edad = document.getElementById("edad_corredor").value;
+  let cedula = document.getElementById("cedula_corredor").value;
+  let fechaFichaMedica = document.getElementById("fecha_vencimiento").value;
+  let tipoDeCorredor = "";
+  if (document.getElementById("elite").checked) {
+    tipoDeCorredor = "Deportista de élite";
+  } else {
+    tipoDeCorredor = "Deportista común";
+  }
+  let corredor = new Corredor(nombre, edad, cedula, fechaFichaMedica, tipoDeCorredor);
+  let fueAgregado = sistema.agregarCorredor(corredor);
+  if (fueAgregado) {
+    agregarElementoCorredor(nombre, cedula);
+  }
+}
+
+// Terminar inscripción
+// Todo estadisticas
